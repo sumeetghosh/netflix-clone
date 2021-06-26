0171080e796f6830b287424c4b66fde6bb68fe89
Sumeet Ghosh <sumeetghosh1990@gmail.com>
2021-06-26 14:24:50 +0530

    Banner Added

---
+++ src/Requests.js
@@ -0,0 +1,25 @@
+const API_KEY = "ae5ac5f2c611f048d64ad292a8f08815";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top-rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocmentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+// const requests = {
+//   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+//   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+//   ​fetchTopRated: `/movie/top-rated?api_key=${API_KEY}&language=en-US`,
+//   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+//   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+//   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+//   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+//   fetchDocmentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+// };
+
+export default requests;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
+import Banner from "../components/Banner/Banner";
 import "./Home.css";
+import requests from "../Requests";
+import Row from "../components/Row/Row";
 
 function Home() {
   return (
     <div className="Home">
       <Nav />
+      <Banner />
     </div>
   );
 }
