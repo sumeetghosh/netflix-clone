6b07434033b55487807ade2dfd88e4a514a2dff3
Sumeet Ghosh <sumeetghosh1990@gmail.com>
2021-06-26 16:06:31 +0530

    Add Rows for each section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,18 @@ function Home() {
     <div className="Home">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movie" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocmentaries} />
     </div>
   );
 }
